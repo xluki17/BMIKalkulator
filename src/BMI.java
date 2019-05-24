8563654af0069eea0298f5b9962d1b539c3f89d3
xluki17 <lukasz.z.wawszczak@gmail.com>
2019-05-24 20:10:23 +0200

    analizownaie wskaźnika

--- src/BMI.java
+++ src/BMI.java
@@ -16,6 +16,16 @@ public class BMI {
         double weight = scanner.nextDouble();
 
         double bmi = weight / (height * height);
-        System.out.println("Twój wskażnik BMI wynosi: " + bmi);
+        System.out.println("Twój wskażnik BMI wynosi:  " + bmi);
+
+        if (bmi < 18.5) {
+            System.out.println("Masz niedowagę");
+        } else if (bmi >= 18.5 && bmi < 25) {
+            System.out.println("Normalna waga");
+        } else if (bmi >= 25 && bmi < 30) {
+            System.out.println("Nadwaga");
+        } else if (bmi >= 30) {
+            System.out.println("Otyły");
+        }
     }
 }
